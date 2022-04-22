e2aa3a68d8966848f3ba2a0b777d949bca273159
OguzDalaslan <104154904+OguzDalaslan@users.noreply.github.com>
2022-04-22 20:48:56 +0300

    CiftmiTekmi

---
+++ CiftmiTekmi/CiftmiTekmi/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CiftmiTekmi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Girilen Sayı Çift mi Tek mi 
+            int sayi, sonuc;
+            Console.WriteLine("Sayı Giriniz");
+            sayi = Convert.ToInt32(Console.ReadLine());
+            sonuc = sayi % 2;
+            if (sonuc == 0)
+                Console.WriteLine("Sayı Çifttir");
+            else
+                Console.WriteLine("Sayı Tektir");
+            Console.ReadKey();
+        }
+    }
+}
